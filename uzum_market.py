6a117690f3f790e8495943870f4779fb00218f80
SHADOW <davronbekn202@gmail.com>
2025-12-22 22:21:29 +0500

    update of market

--- uzum_market.py
+++ uzum_market.py
@@ -4,53 +4,90 @@ import psycopg2
 def get_connection():
     return psycopg2.connect(
         dbname="market",
-        user='postgres',
-        password="yourpassword",
-        host='localhost',
+        user="postgres",
+        password="A0B1D9E2",
+        host="localhost",
         port=5432
     )
 
 
-def create_table_product(user, parol):
+def see():
     connection = get_connection()
     cursor = connection.cursor()
-    cursor.execute("""
-    select * from users where username=%s and password=%s""", (user, parol))
-    user = cursor.fetchone()
-    if user:
-        print(user)
-    else:
-        print("bunady malumot mavjud emas")
 
-
-def add_category(titles):
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute("""insert into category (title)
-    values (%s)""", (titles,))
-    connection.commit()
-    print("yaratildi")
-
-
-# add_category("sabzavodlar")
-
-def get_cat():
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute("""
-    select * from category""")
+    cursor.execute("SELECT * FROM product")
     data = cursor.fetchall()
+
     for d in data:
-        print(f"id {d[0]} title {d[1]}")
+        print(f"id: {d[0]} | title: {d[1]} | price: {d[2]}")
+
+    cursor.close()
+    connection.close()
 
 
-def add_product(title,description,price,quantity,cat_id):
+def add_product(title, price):
     connection = get_connection()
     cursor = connection.cursor()
-    get_cat()
-    cursor.execute("""insert into product  (title,description,price,quantity,cat_id)
-    values (%s,%s,%s,%s,%s)""",(title,description,price,quantity,cat_id))
-    connection.commit()
-    print(f"{title} qoshildi")
 
-add_product("olma","shirin",10000,10,1)
+    cursor.execute("""
+        INSERT INTO product (title, price)
+        VALUES (%s, %s)
+    """, (title, price))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    print("Product added")
+
+
+def update_product(product_id, new_price):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        UPDATE product
+        SET price = %s
+        WHERE id = %s
+    """, (new_price, product_id))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    print("Product updated")
+
+
+def work():
+    while True:
+        print("""
+1 - See products
+2 - Add product
+3 - Update product
+0 - Exit
+        """)
+
+        option = input("Choose: ")
+
+        if option == "1":
+            see()
+
+        elif option == "2":
+            title = input("Enter product title: ")
+            price = float(input("Enter product price: "))
+            add_product(title, price)
+
+        elif option == "3":
+            product_id = int(input("Enter product id: "))
+            new_price = float(input("Enter new price: "))
+            update_product(product_id, new_price)
+
+        elif option == "0":
+            print("Bye")
+            break
+
+        else:
+            print("Invalid option")
+
+work()
+
